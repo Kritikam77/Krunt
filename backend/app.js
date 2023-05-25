01ce1c55caf6d8855ec3a8946f7be2bc9cdab774
Kritika <kritikam77@gmail.com>
2023-05-25 20:42:48 +0530

    2

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
 const path = require("path");
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config({ path: "backend/config/config.env" });
@@ -10,6 +12,13 @@ if (process.env.NODE_ENV !== "production") {
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
+app.use(cors());
+
+// for cors
+app.get('/',(req,res)=>{
+  res.setHeader("Access-Control-Allow-Credentials","true");
+  res.send("API is running..")
+})
 
 // Importing Routes
 const post = require("./routes/post");
@@ -19,10 +28,10 @@ const user = require("./routes/user");
 app.use("/api/v1", post);
 app.use("/api/v1", user);
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+// app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+// });
 
 module.exports = app;
